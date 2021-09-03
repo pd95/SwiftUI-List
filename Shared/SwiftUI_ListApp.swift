0bd8943bd354dfbcb1d373d1ed07b278f894d6c8
Philipp <pd95@users.noreply.github.com>
2021-09-03 20:43:44 +0200

    Initial Commit

---
+++ Shared/SwiftUI_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ListApp.swift
+//  Shared
+//
+//  Created by Philipp on 03.09.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
